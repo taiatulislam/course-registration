cdd367b8442da2074506117aa933c45398652be1
taiatulislam <taiatulislamapon@gmail.com>
2023-09-14 21:16:43 +0100

    credit hour and remaining calculation  added with validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,20 @@ import Courses from './Components/Courses/Courses'
 function App() {
 
   const [bookmarks, setBookmarks] = useState([])
+  const [creditAvailable, setCreditAvailable] = useState(0)
 
   const handleSelect = course => {
     const isAvailable = bookmarks.find(bookmark => bookmark.id === course.id)
-    if (isAvailable) {
-      return alert('Already selected')
+
+    if (creditAvailable + course.credit > 20) {
+      return alert('Credit can not be taken more than 20');
+    }
+    else if (isAvailable) {
+      return alert('Already selected');
     }
     else {
-      return setBookmarks([...bookmarks, course]);
+      setBookmarks([...bookmarks, course]);
+      setCreditAvailable(creditAvailable + course.credit)
     }
 
   }
@@ -23,7 +29,7 @@ function App() {
       <h1 className='text-3xl font-bold text-[#1C1B1B] text-center pb-10'>Course Registration</h1>
       <div className='grid grid-cols-4 gap-5'>
         <div className='col-span-3'><Courses handleSelect={handleSelect}></Courses></div>
-        <div className='col-span-1'><Bookmarks bookmarks={bookmarks}></Bookmarks></div>
+        <div className='col-span-1'><Bookmarks bookmarks={bookmarks} creditAvailable={creditAvailable}></Bookmarks></div>
       </div>
     </div>
   )
--- src/Components/Bookmark/Bookmarks.jsx
+++ src/Components/Bookmark/Bookmarks.jsx
@@ -1,24 +1,25 @@
 import PropTypes from 'prop-types';
 
-const Bookmarks = ({ bookmarks }) => {
+const Bookmarks = ({ bookmarks, creditAvailable }) => {
 
     return (
         <div className="bg-white rounded-lg p-4">
-            <h3 className="text-lg font-bold text-[#2F80ED] border-b-2 py-3">Credit Hour Remaining hr</h3>
+            <h3 className="text-lg font-bold text-[#2F80ED] border-b-2 py-3">Credit Hour Remaining {20 - creditAvailable} hr</h3>
             <h3 className="text-lg font-bold py-4">Course Name: {bookmarks.length}</h3>
             <ol className='list-decimal pl-5 pb-5'>
                 {
-                    bookmarks.map(bookmark => <li key={bookmark.id}>{bookmark.title}</li>)
+                    bookmarks.map(bookmark => <li key={bookmark.id} className='text-[#1C1B1BCC]'>{bookmark.title}</li>)
                 }
             </ol>
-            <h3 className="text-md text-[#1C1B1BCC] font-medium border-y-2 py-3">Total Credit Hour: </h3>
-            <h3 className="text-md text-[#1C1B1BCC] font-medium py-4">Total Price :</h3>
+            <h3 className="text-md text-[#1c1b1bb5] font-semibold border-y-2 py-3">Total Credit Hour: {creditAvailable}</h3>
+            <h3 className="text-md text-[#1c1b1bb5] font-semibold py-4">Total Price :</h3>
         </div>
     );
 };
 
 Bookmarks.propTypes = {
-    bookmarks: PropTypes.array
+    bookmarks: PropTypes.array,
+    creditAvailable: PropTypes.number
 }
 
 export default Bookmarks;
