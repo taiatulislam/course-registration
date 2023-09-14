28ca833ae632856371bb4f623a8b7097515fd6e3
taiatulislam <taiatulislamapon@gmail.com>
2023-09-14 14:44:45 +0100

    images, google fonts added and tailwind configure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='bg-[#F3F3F3]'>
+      <h1 className='text-3xl font-bold text-[#1C1B1B] text-center py-10'>Course Registration</h1>
+    </div>
+  )
+}
+
+export default App
